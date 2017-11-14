798f4ff3f5f5e16bf09b5f03e84a59078612e763
Huy Chau <huy.chau@asnet.com.vn>
2017-11-14 16:16:32 +0700

    Update django config module and web service ports

--- web/config/settings.py
+++ web/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'web.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'web.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
